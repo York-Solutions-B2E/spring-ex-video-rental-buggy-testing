e7b0b48a9c46275030387de3cce085a264b2e7ec
aeharriman <aeharrima@gmail.com>
2023-07-07 09:21:05 -0500

    Begin testing VideoRepository

---
+++ src/test/java/com/example/video_rental/videos/VideoRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.example.video_rental.videos;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VideoRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAllByGenre() {
+    }
+}
